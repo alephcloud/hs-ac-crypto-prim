2684ec03591cdb4ddd1f966e83111227589e145e
Vincent Hanquez <vhanquez@alephcloud.com>
2014-05-02 21:48:53 +0100

    Add support for optionally using OpenSSL ECC backend

--- TWC/Crypto/Ecc/Ops.hs
+++ TWC/Crypto/Ecc/Ops.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE CPP #-}
 module TWC.Crypto.Ecc.Ops
-#if defined(NATIVE)
+#if defined(ECC_NATIVE)
 ( module TWC.Crypto.Ecc.Native ) where import TWC.Crypto.Ecc.Native
-#elif defined(OPENSSL)
+#elif defined(ECC_OPENSSL)
 ( module TWC.Crypto.Ecc.OpenSSL ) where import TWC.Crypto.Ecc.OpenSSL
-#elif defined(SJCL)
+#elif defined(ECC_SJCL)
 ( module TWC.Crypto.Ecc.Sjcl ) where import TWC.Crypto.Ecc.Sjcl
 #else
-#error "undefined backend"
+#error "undefined ECC backend"
 #endif
