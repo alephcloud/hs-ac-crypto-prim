58c47c78e217397c35a20fcc619d035f1f35131b
Vincent Hanquez <vhanquez@pivotmail.com>
2015-01-19 15:16:44 -0800

    push down the infrastructure for signing with ed25519

--- PC/Crypto/Prim/Ed25519.hs
+++ PC/Crypto/Prim/Ed25519.hs
@@ -8,21 +8,42 @@
 --
 
 module PC.Crypto.Prim.Ed25519
-    ( Ed25519.SecretKey
-    , Ed25519.PublicKey
-    , Ed25519.Signature
-    -- * key generation
-    , createKeypair
-    , createSecret
-    , createPublic
-    -- * basic methods
-    , sign
-    , verify
+    ( Ed25519SecretKey
+    , Ed25519PublicKey
+    , Ed25519Signature
     ) where
 
 import Data.ByteString (ByteString)
 import Data.Byteable (Byteable)
 import qualified Crypto.Sign.Ed25519 as Ed25519
+import PC.Crypto.Prim.Class
+import PC.Crypto.Prim.Imports
+
+newtype Ed25519SecretKey = Ed25519SecretKey Ed25519.SecretKey
+    deriving (Show,Bytes,Eq,NFData)
+
+newtype Ed25519PublicKey = Ed25519PublicKey Ed25519.PublicKey
+    deriving (Show,Bytes,Eq,NFData)
+
+instance AsymmetricCrypto Ed25519SecretKey Ed25519PublicKey where
+    assymmetricKeyGenerate                    = Ed25519SecretKey <$> Ed25519.createSecret
+    assymmetricGetPublic (Ed25519SecretKey k) = Ed25519PublicKey $ Ed25519.createPublic k
+
+instance SignatureAlgorithm Ed25519Signature Ed25519SecretKey Ed25519PublicKey where
+    sign   (Ed25519SecretKey sec) = Ed25519.sign' sec
+    verify (Ed25519PublicKey pub) = Ed25519.verify' pub
+
+instance ToACN Ed25519SecretKey where
+    toACN p = [AcnBytes $ toBytes p]
+instance FromACN Ed25519SecretKey where
+    fromACN (AcnBytes b:l) = (\r -> (r, l)) `fmap` fromBytes b
+    fromACN _              = Left "ACN Ed25519SecretKey: invalid sequence"
+
+instance ToACN Ed25519PublicKey where
+    toACN p = [AcnBytes $ toBytes p]
+instance FromACN Ed25519PublicKey where
+    fromACN (AcnBytes b:l) = (\r -> (r, l)) `fmap` fromBytes b
+    fromACN _              = Left "ACN Ed25519PublicKey: invalid sequence"
 
 createKeypair :: IO (Ed25519.PublicKey, Ed25519.SecretKey)
 createKeypair = Ed25519.createKeypair
