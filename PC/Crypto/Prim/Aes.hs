42318c5d8361399c0dda2d169c786e90d33f3db6
Vincent Hanquez <vhanquez@pivotmail.com>
2015-02-04 18:05:52 +0000

    remove old parsers

--- PC/Crypto/Prim/Aes.hs
+++ PC/Crypto/Prim/Aes.hs
@@ -6,44 +6,17 @@
 -- The intellectual property and technical concepts contained herein are
 -- proprietary to PivotCloud and are protected by U.S. and Foreign law.
 --
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE CPP #-}
 module PC.Crypto.Prim.Aes
 #if defined(NATIVE)
-( module PC.Crypto.Prim.Aes.Native, pAesIV, pAes256mac ) where
+( module PC.Crypto.Prim.Aes.Native ) where
 import PC.Crypto.Prim.Aes.Native
 #elif defined(OPENSSL)
-( module PC.Crypto.Prim.Aes.OpenSSL, pAesIV, pAes256mac ) where
+( module PC.Crypto.Prim.Aes.OpenSSL ) where
 import PC.Crypto.Prim.Aes.OpenSSL
 #elif defined(SJCL)
-( module PC.Crypto.Prim.Aes.Sjcl, pAesIV, pAes256mac ) where
+( module PC.Crypto.Prim.Aes.Sjcl ) where
 import PC.Crypto.Prim.Aes.Sjcl
 #else
 #error "undefined backend"
 #endif
-
-import Control.Applicative
-import PC.Bytes.Utils
-import PC.Bytes.ByteArray
-import PC.Bytes.ByteArrayL
-import PC.Crypto.Prim.Sha (pSha512_256, sha512_256Length, Sha512_256Length)
-
-pAesIV :: (BytesL AesIV) => Parser AesIV
-pAesIV = pTakeBytesL <?> "pAesIV"
-
-pAes256mac :: (BytesL AesIV) => Parser (AesIV, BackendByteArray, ByteArrayL Sha512_256Length)
-pAes256mac = (,,)
-    <$> pAesIV
-    <*> pTakeExcept sha512_256Length
-    <*> pSha512_256
-    <?> "pAes256mac"
