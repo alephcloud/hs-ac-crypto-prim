b305d9af32c82490641b74adbdf9142fb31f1948
Vincent Hanquez <vhanquez@pivotmail.com>
2015-03-12 05:40:16 +0000

    update LICENSE and copyright statement

--- Examples/Examples.hs
+++ Examples/Examples.hs
@@ -1,9 +1,28 @@
--- ------------------------------------------------------ --
--- Copyright © 2014 AlephCloud Systems, Inc.
--- ------------------------------------------------------ --
-
+-- Copyright (c) 2013-2015 PivotCloud, Inc.
+--
+-- Examples
+--
+-- Please feel free to contact us at licensing@pivotmail.com with any
+-- contributions, additions, or other feedback; we would love to hear from
+-- you.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may
+-- not use this file except in compliance with the License. You may obtain a
+-- copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+-- License for the specific language governing permissions and limitations
+-- under the License.
+--
+-- |
+-- Module      : Examples
+-- Copyright   : (c) 2013-2015 PivotCloud, Inc
+-- License     : Apache-2, see LICENSE file of the package
+-- Maintainer  : licensing@pivotmail.com
+--
 {-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 -- import PC.Crypto.MAC.Poly1305 (Auth (..))
